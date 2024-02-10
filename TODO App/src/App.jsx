d18f9a7dcea005ac0ab6a8c5710a7804ad62eace
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-10 20:16:00 +0530

    initial ui

--- TODO App/src/App.jsx
+++ TODO App/src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <div className="h-screen w-screen flex  items-center flex-col bg-slate-500">
+        <div className="m-10">
+          <center className="text-5xl font-extrabold">TODO App</center>
+        </div>
+
+        <div>
+          <div class="row">
+            <div class="col-6">
+              <input type="text" className="p-2" placeholder="Enter your TODO" />
+            </div>
+            <div class="col-4">
+              <input type="date" className="p-2" name="" id="" />
+            </div>
+            <div class="col-2">
+              <button class="btn btn-success">ADD</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
